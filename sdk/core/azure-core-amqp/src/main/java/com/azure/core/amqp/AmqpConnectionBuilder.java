89cdafee547f1b9da0311903f9cc8523de3bdf73
Connie <conniey@microsoft.com>
2021-03-04 11:42:03 -0800

    Adding implementation to AmqpConnectionBuilder

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/AmqpConnectionBuilder.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/AmqpConnectionBuilder.java
@@ -3,27 +3,100 @@
 
 package com.azure.core.amqp;
 
+import com.azure.core.amqp.implementation.AzureTokenManagerProvider;
+import com.azure.core.amqp.implementation.ConnectionOptions;
+import com.azure.core.amqp.implementation.MessageSerializer;
+import com.azure.core.amqp.implementation.ReactorConnection;
+import com.azure.core.amqp.implementation.ReactorHandlerProvider;
+import com.azure.core.amqp.implementation.ReactorProvider;
+import com.azure.core.amqp.implementation.StringUtil;
+import com.azure.core.amqp.implementation.TokenManagerProvider;
 import com.azure.core.amqp.models.CbsAuthorizationType;
 import com.azure.core.amqp.models.SslVerifyMode;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.util.ClientOptions;
 import com.azure.core.util.Configuration;
+import com.azure.core.util.CoreUtils;
+import com.azure.core.util.logging.ClientLogger;
+import org.apache.qpid.proton.amqp.transport.ReceiverSettleMode;
+import org.apache.qpid.proton.amqp.transport.SenderSettleMode;
+import reactor.core.scheduler.Schedulers;
 
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
 import java.net.URL;
+import java.time.Duration;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 /**
  * Builder to instantiate an {@link AmqpConnection}.
  */
 public final class AmqpConnectionBuilder {
+    private static final AmqpRetryOptions DEFAULT_RETRY = new AmqpRetryOptions()
+        .setTryTimeout(Duration.ofSeconds(60));
+    private static final Pattern HOST_PORT_PATTERN = Pattern.compile("^[^:]+:\\d+");
+
+    private final ClientLogger logger = new ClientLogger(AmqpConnectionBuilder.class);
+
+    private String activeDirectoryScope;
+    private CbsAuthorizationType authorizationType;
+    private ClientOptions clientOptions;
+    private Configuration configuration;
+    private URL customEndpointAddress;
+    private String fullyQualifiedNamespace;
+    private ProxyOptions proxyOptions;
+    private AmqpRetryOptions retryOptions;
+    private TokenCredential credential;
+    private AmqpTransportType transportType;
+    private SslVerifyMode verifyMode;
+
     /**
-     * Sets the authorization type on the CBS node.
+     * Creates a new builder with default AMQP properties set:
      *
-     * @param authorizationType Authorization type.
+     * <ul>
+     *     <li>{@link #transportType(AmqpTransportType) transportType} of {@link AmqpTransportType#AMQP}</li>
+     *     <li>{@link #verifyMode(SslVerifyMode) verifyMode} of {@link SslVerifyMode#VERIFY_PEER_NAME}</li>
+     *     <li>{@link #retryOptions(AmqpRetryOptions) retryOptions} with 3 attempts and operation timeout of 60s</li>
+     * </ul>
+     */
+    public AmqpConnectionBuilder() {
+        this.transportType = AmqpTransportType.AMQP;
+        this.verifyMode = SslVerifyMode.VERIFY_PEER_NAME;
+        this.retryOptions = DEFAULT_RETRY;
+    }
+
+    /**
+     * Sets the active directory scope to use when authorizing with the CBS node. Required when {@link
+     * #authorizationType(CbsAuthorizationType) authorizationType} is {@link CbsAuthorizationType#JSON_WEB_TOKEN}.
+     *
+     * @param activeDirectoryScope The active directory scope to use.
      *
      * @return The updated {@link AmqpConnectionBuilder} object.
+     *
+     * @throws NullPointerException if {@code activeDirectoryScope} is null.
+     */
+    public AmqpConnectionBuilder activeDirectoryScope(String activeDirectoryScope) {
+        this.activeDirectoryScope = Objects.requireNonNull(activeDirectoryScope,
+            "'activeDirectoryScope' cannot be null.");
+        return this;
+    }
+
+    /**
+     * Sets the authorization type on the CBS node. If {@code authorizationType} is {@link
+     * CbsAuthorizationType#JSON_WEB_TOKEN JSON_WEB_TOKEN}, then the {@link #activeDirectoryScope(String)
+     * activeDirectoryScope} must be set.
+     *
+     * @param authorizationType Authorization type to use with the CBS node.
+     *
+     * @return The updated {@link AmqpConnectionBuilder} object.
+     *
+     * @throws NullPointerException if {@code authorizationType} is null.
      */
     public AmqpConnectionBuilder authorizationType(CbsAuthorizationType authorizationType) {
+        this.authorizationType = Objects.requireNonNull(authorizationType, "'authorizationType' cannot be null.");
         return this;
     }
 
@@ -37,10 +110,24 @@ public final class AmqpConnectionBuilder {
      *
      * @return The updated {@link AmqpConnectionBuilder} object.
      *
+     * @throws NullPointerException if {@code clientOptions} is null.
      * @see <a href="https://azure.github.io/azure-sdk/general_azurecore.html#telemetry-policy">Azure Core: Telemetry
      *     policy</a>
      */
     public AmqpConnectionBuilder clientOptions(ClientOptions clientOptions) {
+        this.clientOptions = Objects.requireNonNull(clientOptions, "'clientOptions' cannot be null.");
+        return this;
+    }
+
+    /**
+     * Sets the credential used to authenticate requests to the AMQP message broker.
+     *
+     * @param credential {@link TokenCredential} to be used for authentication.
+     *
+     * @return The updated {@link AmqpConnectionBuilder} object.
+     */
+    public AmqpConnectionBuilder credential(TokenCredential credential) {
+        this.credential = Objects.requireNonNull(credential, "'credential' cannot be null.");
         return this;
     }
 
@@ -55,17 +142,7 @@ public final class AmqpConnectionBuilder {
      * @return The updated {@link AmqpConnectionBuilder} object.
      */
     public AmqpConnectionBuilder configuration(Configuration configuration) {
-        return this;
-    }
-
-    /**
-     * Connection string for the AMQP message broker.
-     *
-     * @param connectionString Connection string for the AMQP message broker.
-     *
-     * @return The updated {@link AmqpConnectionBuilder} object.
-     */
-    public AmqpConnectionBuilder connectionString(String connectionString) {
+        this.configuration = Objects.requireNonNull(configuration, "'configuration' cannot be null.");
         return this;
     }
 
@@ -84,6 +161,18 @@ public final class AmqpConnectionBuilder {
      * @throws IllegalArgumentException if {@code customEndpointAddress} cannot be parsed into a valid {@link URL}.
      */
     public AmqpConnectionBuilder customEndpointAddress(String customEndpointAddress) {
+        if (customEndpointAddress == null) {
+            this.customEndpointAddress = null;
+            return this;
+        }
+
+        try {
+            this.customEndpointAddress = new URL(customEndpointAddress);
+        } catch (MalformedURLException e) {
+            throw logger.logExceptionAsError(
+                new IllegalArgumentException(customEndpointAddress + ": is not a valid URL.", e));
+        }
+
         return this;
     }
 
@@ -95,6 +184,12 @@ public final class AmqpConnectionBuilder {
      * @return The updated {@link AmqpConnectionBuilder} object.
      */
     public AmqpConnectionBuilder fullyQualifiedNamespace(String fullyQualifiedNamespace) {
+        if (CoreUtils.isNullOrEmpty(fullyQualifiedNamespace)) {
+            throw logger.logExceptionAsError(new IllegalArgumentException(
+                "'fullyQualifiedNamespace' cannot be null or an empty string."));
+        }
+
+        this.fullyQualifiedNamespace = fullyQualifiedNamespace;
         return this;
     }
 
@@ -105,8 +200,11 @@ public final class AmqpConnectionBuilder {
      * @param proxyOptions The proxy configuration to use.
      *
      * @return The updated {@link AmqpConnectionBuilder} object.
+     *
+     * @throws NullPointerException if {@code proxyOptions} is null.
      */
     public AmqpConnectionBuilder proxyOptions(ProxyOptions proxyOptions) {
+        this.proxyOptions = Objects.requireNonNull(proxyOptions, "'proxyOptions' cannot be null.");
         return this;
     }
 
@@ -116,19 +214,11 @@ public final class AmqpConnectionBuilder {
      * @param retryOptions The retry options to use.
      *
      * @return The updated {@link AmqpConnectionBuilder} object.
+     *
+     * @throws NullPointerException if {@code retryOptions} is null.
      */
     public AmqpConnectionBuilder retryOptions(AmqpRetryOptions retryOptions) {
-        return this;
-    }
-
-    /**
-     * Sets the credential used to authenticate requests to the AMQP message broker.
-     *
-     * @param tokenCredential {@link TokenCredential} to be used for authentication.
-     *
-     * @return The updated {@link AmqpConnectionBuilder} object.
-     */
-    public AmqpConnectionBuilder tokenCredential(TokenCredential tokenCredential) {
+        this.retryOptions = Objects.requireNonNull(retryOptions, "'retryOptions' cannot be null.");
         return this;
     }
 
@@ -139,8 +229,11 @@ public final class AmqpConnectionBuilder {
      * @param transportType The transport type to use.
      *
      * @return The updated {@link AmqpConnectionBuilder} object.
+     *
+     * @throws NullPointerException if {@code transportType} is null.
      */
     public AmqpConnectionBuilder transportType(AmqpTransportType transportType) {
+        this.transportType = Objects.requireNonNull(transportType, "'transportType' cannot be null.");
         return this;
     }
 
@@ -150,8 +243,11 @@ public final class AmqpConnectionBuilder {
      * @param verifyMode The verification mode.
      *
      * @return The updated {@link AmqpConnectionBuilder} object.
+     *
+     * @throws NullPointerException if {@code transportType} is null.
      */
     public AmqpConnectionBuilder verifyMode(SslVerifyMode verifyMode) {
+        this.verifyMode = Objects.requireNonNull(verifyMode, "'verifyMode' cannot be null.");
         return this;
     }
 
@@ -161,6 +257,82 @@ public final class AmqpConnectionBuilder {
      * @return A new AMQP connection.
      */
     public AmqpConnection buildConnection() {
-        return null;
+        final ConnectionOptions connectionOptions = getConnectionOptions();
+
+        final TokenManagerProvider tokenManagerProvider = new AzureTokenManagerProvider(
+            connectionOptions.getAuthorizationType(), connectionOptions.getFullyQualifiedNamespace(),
+            activeDirectoryScope);
+        final ReactorProvider provider = new ReactorProvider();
+        final ReactorHandlerProvider handlerProvider = new ReactorHandlerProvider(provider);
+
+        final String connectionId = StringUtil.getRandomString("MF");
+        logger.info("connectionId[{}]: Emitting a single connection.", connectionId);
+
+        final MessageSerializer serializer = new AmqpAnnotatedMessageSerializer();
+
+        return new ReactorConnection(connectionId, connectionOptions, provider, handlerProvider, tokenManagerProvider,
+            serializer, SenderSettleMode.SETTLED, ReceiverSettleMode.SECOND);
+    }
+
+    private ConnectionOptions getConnectionOptions() {
+        if (authorizationType == null) {
+            throw logger.logExceptionAsError(new IllegalStateException(
+                "'authorizationType' is required. Set it in the builder."));
+        }
+
+        // If the proxy has been configured by the user but they have overridden the TransportType with
+        // something that is not AMQP_WEB_SOCKETS.
+        final ProxyOptions proxyToUse = proxyOptions != null ? proxyOptions : getDefaultProxyConfiguration();
+
+        if (proxyToUse != null && proxyToUse.isProxyAddressConfigured()
+            && transportType != AmqpTransportType.AMQP_WEB_SOCKETS) {
+            throw logger.logExceptionAsError(new IllegalArgumentException(
+                "Cannot use a proxy when TransportType is not AMQP Web Sockets."));
+        }
+
+        final ClientOptions clientOptionsToUse = clientOptions != null ? clientOptions : new ClientOptions();
+
+        if (customEndpointAddress == null) {
+            return new ConnectionOptions(fullyQualifiedNamespace, credential, authorizationType, transportType,
+                retryOptions, proxyToUse, Schedulers.elastic(), clientOptionsToUse, verifyMode);
+        } else {
+            return new ConnectionOptions(fullyQualifiedNamespace, credential, authorizationType, transportType,
+                retryOptions, proxyToUse, Schedulers.elastic(), clientOptionsToUse, verifyMode,
+                customEndpointAddress.getHost(), customEndpointAddress.getPort());
+        }
+    }
+
+    private ProxyOptions getDefaultProxyConfiguration() {
+        ProxyAuthenticationType authentication = ProxyAuthenticationType.NONE;
+        if (proxyOptions != null) {
+            authentication = proxyOptions.getAuthentication();
+        }
+
+        final Configuration configurationToUse = this.configuration == null
+            ? Configuration.getGlobalConfiguration()
+            : this.configuration;
+        final String proxyAddress = configurationToUse.get(Configuration.PROPERTY_HTTP_PROXY);
+
+        if (CoreUtils.isNullOrEmpty(proxyAddress)) {
+            return ProxyOptions.SYSTEM_DEFAULTS;
+        }
+
+        if (HOST_PORT_PATTERN.matcher(proxyAddress.trim()).find()) {
+            final String[] hostPort = proxyAddress.split(":");
+            final String host = hostPort[0];
+            final int port = Integer.parseInt(hostPort[1]);
+
+            final Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port));
+            final String username = configurationToUse.get(ProxyOptions.PROXY_USERNAME);
+            final String password = configurationToUse.get(ProxyOptions.PROXY_PASSWORD);
+
+            return new ProxyOptions(authentication, proxy, username, password);
+        } else {
+            com.azure.core.http.ProxyOptions coreProxyOptions = com.azure.core.http.ProxyOptions
+                .fromConfiguration(configurationToUse);
+
+            return new ProxyOptions(authentication, new Proxy(coreProxyOptions.getType().toProxyType(),
+                coreProxyOptions.getAddress()), coreProxyOptions.getUsername(), coreProxyOptions.getPassword());
+        }
     }
 }
