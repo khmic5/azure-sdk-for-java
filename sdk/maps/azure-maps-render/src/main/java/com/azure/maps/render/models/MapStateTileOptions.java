4dce675f32f8466d525b8e706d8d629077ddf1a8
Katherine Hsu <weitzuhsu@microsoft.com>
2022-02-15 19:15:52 +0800

    fixed builder

--- sdk/maps/azure-maps-render/src/main/java/com/azure/maps/render/models/MapStateTileOptions.java
+++ sdk/maps/azure-maps-render/src/main/java/com/azure/maps/render/models/MapStateTileOptions.java
@@ -1,5 +1,7 @@
 package com.azure.maps.render.models;
 
+import com.azure.maps.render.implementation.models.TileIndex;
+
 public class MapStateTileOptions {
     private String statesetId;
     private TileIndex tileIndex;
