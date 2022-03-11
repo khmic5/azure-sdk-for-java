9b59ca0d9ccc446453afaab3208c3dfc2262c4cb
Daniel Rocha <daroch@microsoft.com>
2022-03-11 13:42:19 +0800

    Added Utility package

---
+++ sdk/maps/azure-maps-weather/src/main/java/com/azure/maps/weather/implementation/helpers/Utility.java
@@ -0,0 +1,32 @@
+package com.azure.maps.weather.implementation.helpers;
+
+import java.io.ByteArrayOutputStream;
+
+import com.azure.core.models.GeoCollection;
+import com.azure.core.models.GeoPolygon;
+import com.azure.core.serializer.json.jackson.JacksonJsonSerializer;
+import com.azure.core.serializer.json.jackson.JacksonJsonSerializerProvider;
+import com.azure.core.util.serializer.TypeReference;
+import com.azure.maps.weather.implementation.models.GeoJsonGeometry;
+import com.azure.maps.weather.implementation.models.GeoJsonGeometryCollection;
+
+public class Utility {
+    private static final JacksonJsonSerializer serializer = new JacksonJsonSerializerProvider()
+        .createInstance();
+
+    /**
+     * Converts a {@link GeoCollection} into a private {@link GeoJsonGeometryCollection}.
+     *
+     * @param object
+     * @return
+     */
+    public static GeoPolygon toGeoPolygon(GeoJsonGeometry geometry) {
+        // serialize to GeoJson
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        serializer.serialize(baos, geometry);
+
+        // deserialize into GeoPolygon
+        final TypeReference<GeoPolygon> typeReference = new TypeReference<GeoPolygon>(){};
+        return serializer.deserializeFromBytes(baos.toByteArray(), typeReference);
+    }
+}
