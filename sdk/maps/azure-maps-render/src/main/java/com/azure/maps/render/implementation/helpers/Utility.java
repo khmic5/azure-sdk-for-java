fe85cb1c560c7fc8e2063681a977febc861d4d69
Katherine Hsu <weitzuhsu@microsoft.com>
2022-05-23 14:24:19 +0800

    update remove not null function in utility

--- sdk/maps/azure-maps-render/src/main/java/com/azure/maps/render/implementation/helpers/Utility.java
+++ sdk/maps/azure-maps-render/src/main/java/com/azure/maps/render/implementation/helpers/Utility.java
@@ -5,23 +5,7 @@ import java.util.Locale;
 import com.azure.maps.render.implementation.models.IncludeText;
 
 public class Utility {
-    private static final String ARGUMENT_NULL_OR_EMPTY =
-        "The argument must not be null or an empty string. Argument name: %s.";
-        
     public static IncludeText toIncludeTextPrivate(boolean includeText) {
         return IncludeText.fromString(String.valueOf(includeText));
-    }
-
-    /**
-     * Asserts that a value is not {@code null}.
-     *
-     * @param param Name of the parameter
-     * @param value Value of the parameter
-     * @throws NullPointerException If {@code value} is {@code null}
-     */
-    public static void assertNotNull(final String param, final Object value) {
-        if (value == null) {
-            throw new NullPointerException(String.format(Locale.ROOT, ARGUMENT_NULL_OR_EMPTY, param));
-        }
     }
 }
